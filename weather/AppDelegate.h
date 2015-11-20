4fd166b500d3015d6490d497c7280fa6cf3b8c69
traidocthan47 <traidocthan47@hackers-MacBook-Pro.local>
2015-11-20 00:10:47 -0800

    Initial Commit

---
+++ weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather
+//
+//  Created by traidocthan47 on 11/20/15.
+//  Copyright (c) 2015 traidocthan47. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
